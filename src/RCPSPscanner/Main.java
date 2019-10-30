535229c01d880edd30782c3244e466acb8769abd
Thx And Bye <72bltj1bif@hft-stuttgart.de>
2019-10-30 20:41:05 +0100

    calculateEligibleJobs now a single stream

--- src/RCPSPscanner/Main.java
+++ src/RCPSPscanner/Main.java
@@ -5,21 +5,23 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.Objects;
 import java.util.Optional;
 
 public class Main {
 
-    private static ArrayList<Job> calculateEligableJobs(Job[] jobs,LinkedList<Integer> plannedJobs){
-        ArrayList<Job> newEligableJobs = new ArrayList<>();
+    private static ArrayList<Job> calculateEligibleJobs(Job[] jobs,LinkedList<Integer> plannedJobs){
+        ArrayList<Job> newEligibleJobs = new ArrayList<>();
 
         for (Integer jobNumber : plannedJobs) {
-            Job.getJob(jobs, jobNumber).nachfolger.forEach(x -> newEligableJobs.add(Job.getJob(jobs, x)));
+            Objects.requireNonNull(
+                    Job.getJob(jobs, jobNumber)).nachfolger.stream()
+                    .map(x -> Job.getJob(jobs, x))
+                    .filter(Objects::nonNull).filter(x -> plannedJobs.containsAll(x.vorgaenger))
+                    .forEach(newEligibleJobs::add);
         }
-        newEligableJobs.forEach(x -> {
-            if(!plannedJobs.containsAll(x.vorgaenger)) newEligableJobs.remove(x);
-        });
 
-        return newEligableJobs;
+        return newEligibleJobs;
     }
 
 
@@ -48,8 +50,8 @@ public class Main {
         Resource[] res = Resource.read(new File("j12046_8.sm"));
 
 
-        for (int i = 0; i < jobs.length; i++) {
-            jobs[i].calculatePredecessors(jobs);
+        for (Job job : jobs) {
+            job.calculatePredecessors(jobs);
         }
         auslesen(jobs);
         auslesen(res);
@@ -60,34 +62,34 @@ public class Main {
 
     private static void auslesen(Job[] jobs) {
         int gesamtDauer = 0;
-        for (int i = 0; i < jobs.length; i++) {
-            gesamtDauer += jobs[i].dauer();
+        for (Job job : jobs) {
+            gesamtDauer += job.dauer();
 
-            System.out.print("Nummer: " + jobs[i].nummer() + "     |    ");
+            System.out.print("Nummer: " + job.nummer() + "     |    ");
             System.out.print("Nachfolger: ");
-            ArrayList<Integer> nachfolger = jobs[i].nachfolger();
-            for (int j = 0; j < nachfolger.size(); j++) {
-                System.out.print(" " + nachfolger.get(j) + " ");
+            ArrayList<Integer> nachfolger = job.nachfolger();
+            for (Integer integer : nachfolger) {
+                System.out.print(" " + integer + " ");
 
             }
             System.out.print(" Vorgaenger: ");
-            ArrayList<Integer> vorgaenger = jobs[i].vorgaenger();
-            for (int j = 0; j < vorgaenger.size(); j++) {
-                System.out.print(" " + vorgaenger.get(j) + " ");
+            ArrayList<Integer> vorgaenger = job.vorgaenger();
+            for (Integer integer : vorgaenger) {
+                System.out.print(" " + integer + " ");
 
             }
             System.out.print("     |    ");
-            System.out.print("Dauer: " + jobs[i].dauer() + "     |    ");
-            System.out.println("R1: " + jobs[i].verwendeteResource(0) + "  R2: " + jobs[i].verwendeteResource(1) +
-                    "  R3: " + jobs[i].verwendeteResource(2) + "  R4: " + jobs[i].verwendeteResource(3));
+            System.out.print("Dauer: " + job.dauer() + "     |    ");
+            System.out.println("R1: " + job.verwendeteResource(0) + "  R2: " + job.verwendeteResource(1) +
+                    "  R3: " + job.verwendeteResource(2) + "  R4: " + job.verwendeteResource(3));
         }
         System.out.println("T = " + gesamtDauer);
     }
 
     private static void auslesen(Resource[] resource) {
-        for (int i = 0; i < resource.length; i++) {
-            System.out.print("Resource: " + resource[i].nummer() + "     |    ");
-            System.out.println("Verfügbarkeit: " + resource[i].maxVerfuegbarkeit());
+        for (Resource value : resource) {
+            System.out.print("Resource: " + value.nummer() + "     |    ");
+            System.out.println("Verfügbarkeit: " + value.maxVerfuegbarkeit());
         }
     }
 
